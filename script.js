00361b4960aba3ced71b201bfab72abd6e7ef021
RudraKaradkhelkar <159210831+RudraKaradkhelkar@users.noreply.github.com>
2024-07-02 21:35:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+$( document ).ready(function() {
+    const calcText="#calculator-window > span";
 
+    $(calcText).html("0");
+
+    console.log( "ready!" );
+    
+    $(":button").on("click", function(){
+        if($(this).attr("data-action")!==undefined){
+            console.log($(this).attr("data-action"));
+            $(calcText).html($(this).attr("data-action")); // For testing if calculator window works
+        }  else{
+           console.log($(this).html());
+           $(calcText).html($(this).html()); // For testing if calculator window works
+        }
+    });
+
+
+});
