31d85f3f5313dc1d7e65fc5603755c0b2648c1e7
RudraKaradkhelkar <159210831+RudraKaradkhelkar@users.noreply.github.com>
2024-07-07 20:21:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
 
     console.log( "ready!" );
     
-    $(":button").on("click", function(){
+    $(":button").on("click", function(e){
         if($(this).attr("data-action")!==undefined){
             console.log($(this).attr("data-action"));
             $(calcText).html($(this).attr("data-action")); // For testing if calculator window works
@@ -14,5 +14,32 @@ $( document ).ready(function() {
            $(calcText).html($(this).html()); // For testing if calculator window works
         }
     });
-    
+
+    function calculate(num1,action,num2){
+        var result;
+
+        switch(action){
+            case "add":
+                result = parseFloat(num1) + parseFloat(num2);
+                break;
+            case "subtract":
+                result = parseFloat(num1) - parseFloat(num2);
+                break;
+            case "multiply":
+                result = parseFloat(num1) * parseFloat(num2);
+                break;
+            case "divide":
+                result = parseFloat(num1) / parseFloat(num2);
+                break;
+            default:
+                console.log("Error: "+action+" is an undefined action");
+        }
+        return result;
+    }
+    console.log(calculate(3,"multiply",2));
+    console.log(calculate(4,"subtract",1));
+    console.log(calculate(7,"add",7));
+    console.log(calculate(2,"divide",2));
+    console.log(calculate(2,"equal",2));
 });
+
